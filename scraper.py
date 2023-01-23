e31e7f83d6560a97c7d7c388a0d8f1417ea6c2c6
Tai Cao <Henshin21@yahoo.pl>
2023-01-23 16:53:23 +0100

    added codes from kursy_walut into scraper

--- scraper.py
+++ scraper.py
@@ -1,3 +1,13 @@
 import requests
+import csv
+
+
 response = requests.get("http://api.nbp.pl/api/exchangerates/tables/C?format=json")
 data = response.json()
+
+with open('currency_rates.csv', mode='w') as currency_file:
+    fieldnames = ['currency', 'code', 'bid', 'ask']
+    writer = csv.DictWriter(currency_file, fieldnames=fieldnames)
+    writer.writeheader()
+    for rate in data[0]['rates']:
+        writer.writerow({'currency': rate['currency'], 'code': rate['code'], 'bid': rate['bid'], 'ask': rate['ask']})
